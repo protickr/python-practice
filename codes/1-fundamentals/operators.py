21f66591ee7c7347f3cf8c6ace3711b838e42bd9
Protick Roy <protickrr@gmail.com>
2022-07-08 13:09:46 +0600

    Arithmatic, comparison and compound assign operators

---
+++ codes/1-fundamentals/operators.py
@@ -0,0 +1,140 @@
+# integer is a whole number 
+# float is a decimal number 
+
+# integer
+# <class 'int'>
+# num = 10
+# print(num)
+# print(type(num))
+
+# float 
+# <class 'float'>
+# num = 10.6
+# print(num)
+# print(type(num))
+
+# type casting / converison 
+# num_1 = '10'
+# num_2 = '10.5'
+# string to int, float
+# print(int(num_1), float(num_2))
+# 10 10.5
+
+
+
+# helper function 
+#
+# absolute value
+# abs() 
+# num_1 = -15
+# print(abs(num_1))
+# 15
+#
+# round to nearest int or float 
+# nearest int 
+# num = 10.6
+# print(round(num))
+# 11
+# 
+# round to nearest float 
+# num = 10.65
+# round to 1 decimal place
+# print(round(num, 1))
+# 10.7
+
+
+
+
+# arithmatic operator 
+# num_1 = 10
+# num_2 = 3
+
+# parenthesis
+# print( (num_1 * 2) * num_2)
+# 60
+
+# exponentiation
+# print(num_1 ** num_2)
+# 10 to the power of 3 = 10 * 10 * 10 == 1000
+
+# multiplication 
+# print(num_1 * num_2)
+# 30
+
+# divisoin 
+# print(num_1 / num_2)
+# in python 3, 10 / 3 == 3.3333333333333335
+# in python 2, 10 / 3 == 3
+
+# floor division 
+# print(num_1 // num_2)
+# 10 // 3 == 3
+
+# remainder operator
+# print(num_1 % num_2)
+# 10 % 3 == 1 
+
+# addition 
+# print(num_1 + num_2)
+# 10 + 3 == 13
+
+# substraction 
+# print(num_1 - num_2)
+# 10 - 3 == 7
+
+# arithmatic operator precendence order 
+# Parenthesis Exponentiation Multiplication Division Addition Substraction
+# PEMDAS
+
+
+
+
+# Comparison operator 
+# ==, !=, >, <, >=, <=
+# num_1 = 10
+# num_2 = 3
+
+# equal 
+# print( num_1 == num_2)
+# 10 == 3 ? False 
+
+# not equal 
+# print(num_1 != num_2)
+# 10 != 3 ? True
+
+# greater than 
+# print(num_1 > num_2)
+# 10 > 3 ? True
+
+# less than 
+# print(num_1 < num_2)
+# 10 < 3 ? False
+
+# greater than or equal 
+# print(num_1 >= num_2)
+# 10 >= 3 ? True
+
+# less than or equal 
+# print( num_1 <= num_2)
+# 10 <= 3 ? False
+
+# assignment and compound assignment operator 
+
+# = (assignment ) operator 
+# num = 10
+
+# compound assignemnt operators 
+# num += 1
+# print(num)
+# num = num + 1 == 11
+
+
+# num **= 2
+# print(num)
+# num = num ** 2 
+# num = 10 ** 2
+# num = 100
+
+# similerly,  
+# -=, *=, /=, //=, %=, **= 
+
