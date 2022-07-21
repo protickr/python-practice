88b7e57c13c0bcc4ac854eecb4d21e36cd1ad35a
Protick Roy <protickrr@gmail.com>
2022-07-22 03:01:34 +0600

    Iterator practice with class

---
+++ codes/3-advanced_functions/iterator_practice.py
@@ -0,0 +1,22 @@
+class Sentence:
+    def __init__(self, sentence) -> None:
+        self.sentence = sentence
+        self.words = self.sentence.strip().split(' ')
+        self.length = len(self.words)
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index < self.length:
+            cur_word = self.words[self.index]
+            self.index += 1
+            return cur_word 
+        else:
+            raise StopIteration
+        
+
+words = Sentence('This is a sentence')
+for word in words: 
+    print(word)
