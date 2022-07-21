c8bd0ef210060db07ee430ae02605ec887173ac7
Protick Roy <protickrr@gmail.com>
2022-07-22 03:04:05 +0600

    Iterator practice with class and generator function

--- codes/3-advanced_functions/iterator_practice.py
+++ codes/3-advanced_functions/iterator_practice.py
@@ -1,22 +1,33 @@
-class Sentence:
-    def __init__(self, sentence) -> None:
-        self.sentence = sentence
-        self.words = self.sentence.strip().split(' ')
-        self.length = len(self.words)
-        self.index = 0
+# class Sentence:
+#     def __init__(self, sentence) -> None:
+#         self.sentence = sentence
+#         self.words = self.sentence.strip().split(' ')
+#         self.length = len(self.words)
+#         self.index = 0
 
-    def __iter__(self):
-        return self
+#     def __iter__(self):
+#         return self
 
-    def __next__(self):
-        if self.index < self.length:
-            cur_word = self.words[self.index]
-            self.index += 1
-            return cur_word 
-        else:
-            raise StopIteration
+#     def __next__(self):
+#         if self.index < self.length:
+#             cur_word = self.words[self.index]
+#             self.index += 1
+#             return cur_word 
+#         else:
+#             raise StopIteration
         
 
-words = Sentence('This is a sentence')
+# words = Sentence('This is a sentence')
+# for word in words: 
+#     print(word)
+
+
+
+def sentence(sent):
+    words = sent.strip().split(' ')
+    for word in words:
+        yield word
+
+words = sentence('')
 for word in words: 
     print(word)
